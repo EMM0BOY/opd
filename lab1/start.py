a7dae84469d074b3bbf9e2b23c32d583dcc817b2
EMM0BOY <127761021+EMM0BOY@users.noreply.github.com>
2023-04-15 11:31:05 +0600

    Update start.py

--- lab1/start.py
+++ lab1/start.py
@@ -1,20 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import xlsxwriter
-import math
 data = [['Название', 'Автор', 'Цена']]
 slovo = input("Введите запрос: ")
 def pagenation():
     url = f"https://www.chitai-gorod.ru/search?phrase={slovo}"
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
-    pages= soup.find('p', class_='search-page__found-message').text.strip().split()
-    return math.ceil(int(pages[-2])/48)
+    pages=soup.find('div', class_='pagination__wrapper').text.strip().split()
+    return int(pages[-1])
 pages=pagenation()
 print(f"мы нашли {pages} страниц")
 def parse():
-    for p in range(1,pages+1):
-        url = f"https://www.chitai-gorod.ru/search?phrase={slovo}&page={p}"
+    for p in range(1, pages + 1):
+        url=f"https://www.chitai-gorod.ru/search?phrase={slovo}&page={p}"
         page = requests.get(url)
         print(page.status_code)
         soup = BeautifulSoup(page.text, "html.parser")
@@ -28,9 +27,9 @@ def parse():
                 article_price = 'Not Found'
             data.append([article_nazvanie, article_avtor, article_price])
     return data
-ans=parse()
+ans = parse()
 def writer(data):
-    with xlsxwriter.Workbook('res.xlsx') as workbook:
+    with xlsxwriter.Workbook('resuslt.xlsx') as workbook:
         worksheet = workbook.add_worksheet()
         for row_num, info in enumerate(data):
             worksheet.write_row(row_num, 0, info)
